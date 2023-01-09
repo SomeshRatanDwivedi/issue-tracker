2b1d7a076230c368fdfe7aed1eeb5102ecc97ce0
somesh dwivedi <Pamli.Basak@dxglobal.com-at-376039757673>
2023-01-09 16:26:04 +0530

    change in production mode

--- config/environment.js
+++ config/environment.js
@@ -3,18 +3,16 @@
 const development={
     nams:'development',
     asset_path:'./assets',
-    db_name:"bug-tracker"  
 }
 
 
 const production={
     name:'production',
     asset_path:process.env.ERROR_TRACKER_ASSET_PATH,
-    db_name:process.env.ERROR_TRACKER_DB_NAME
 }
 
 
 
 
 
-module.exports=development
+module.exports=eval(process.env.ERROR_TRACKER_ENVIRONMENT)==undefined?development:eval(process.env.ERROR_TRACKER_ENVIRONMENT);
